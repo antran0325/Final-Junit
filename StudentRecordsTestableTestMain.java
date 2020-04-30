d143f62bb2aab834f3641d2e955d83c304c38437
antran0325 <36683922+antran0325@users.noreply.github.com>
2020-04-30 19:46:39 -0400

    Delete StudentRecordsTestableTestMain.java

--- StudentRecordsTestableTestMain.java
+++
@@ -1,6 +0,0 @@
-public class StudentRecordsTestableTestMain {
-    public static void main(String[]args){
-        StudentRecordsTestableTest test = new StudentRecordsTestableTest();
-        test.beforeDrop();
-    }
-}
